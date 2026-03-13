60a15f150bd4fdb301afb1c5983a8bb2030ec0ec
angelpoon <dsp.angel@gmail.com>
2026-03-13 16:38:26 -0700

    Initial Commit

---
+++ jarie/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  jarie
+//
+//  Created by Angel Poon on 3/13/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ jarieTests/jarieTests.swift
@@ -0,0 +1,17 @@
+//
+//  jarieTests.swift
+//  jarieTests
+//
+//  Created by Angel Poon on 3/13/26.
+//
+
+import Testing
+@testable import jarie
+
+struct jarieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
